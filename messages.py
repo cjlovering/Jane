d237b1b94b9f7077e0c146972404cfa2bb6e13dd
1nkling <dlsong@wpi.edu>
2017-01-14 17:27:07 -0500

    sender action

--- messages.py
+++ messages.py
@@ -29,6 +29,7 @@ def send_message(recipient_id, message_text):
         "recipient": {
             "id": recipient_id
         },
+        "sender_action":"typing_on"
         "message": {
             "text": message_text
         }
