f1bd155d664b6f2da14b0d0f68f82b56e647aaf0
cjlovering <cjlovering@130.215.14.76>
2017-01-13 20:35:18 -0500

    test

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def verify():
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
-    return "Hello world", 200
+    return "<h1> Hello world </h1>", 200
 
 
 @app.route('/', methods=['POST'])
