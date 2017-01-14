825abc9df900116931f9d6332e2b13cfe2900b6f
saahilclaypool <saahil@claypools.org>
2017-01-14 14:34:20 -0500

    more consistent search results

--- ImageSearch.py
+++ ImageSearch.py
@@ -4,7 +4,7 @@ from random import randint
 
 def getURL (search_term):
     search_service = PyMsCognitiveImageSearch('5a41108e4cee4b4297e3887cff817ab3' , search_term)
-    res = randint(0,50) 
+    res = randint(0,20) 
     first_fifty_result = search_service.search(limit=50, format='json') #1-50
     return first_fifty_result[res].content_url
 
