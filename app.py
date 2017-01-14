df22fc605f0b02058ee4bf9724bc03125e53f699
saahilclaypool <saahil@claypools.org>
2017-01-13 21:21:46 -0500

    send image

--- app.py
+++ app.py
@@ -79,8 +79,16 @@ def send_message(recipient_id, message_text):
             "id": recipient_id
         },
         "message": {
-            "text": message_text
-        }
+            "attachment": {
+                "type": "image",
+                "payload":{
+                    "url": "https://petersapparel.parseapp.com/img/shirt.png" 
+                    } 
+                }
+            }
+        # "message": {
+            # "text": message_text
+        # }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
