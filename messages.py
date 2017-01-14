ad446122243a2b4e5360fe58d11281e429ff5c94
1nkling <dlsong@wpi.edu>
2017-01-14 17:44:42 -0500

    resp bubble

--- messages.py
+++ messages.py
@@ -31,7 +31,7 @@ def send_message(recipient_id, message_text):
         },
         "message": {
             "text": message_text
-        }
+        },
         "sender_action":"typing_on"
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
