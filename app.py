7ad1ba3ae9baa31377fd738f74e6b34caa775446
saahilclaypool <saahil@claypools.org>
2017-01-13 20:43:59 -0500

    add try around dict access

--- app.py
+++ app.py
@@ -37,7 +37,11 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    try:
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                    except: 
+                        message_text = "BAD VALUE"
+
                     handle_message(sender_id, message_text)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
