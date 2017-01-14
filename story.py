06c014864e28f431ea8bba0f8bc910a2e4155b60
saahilclaypool <saahil@claypools.org>
2017-01-14 15:39:24 -0500

    sytax charlie sux

--- story.py
+++ story.py
@@ -5,7 +5,7 @@ from story_gen import story_gen
 def handle_story(state, sender_id, message_in):
     # build story
     potential = ["Let me think...", "I've got a good one...", "I made this one up myself...", "This happened a long time ago..."]
-    send_message(sender_id, potential[random.randint(0,3))
+    send_message(sender_id, potential[random.randint(0,3)])
     story = story_gen(message_in)
     # send story
     # format story
