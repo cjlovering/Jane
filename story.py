b2701e23e6f85150a3770bdc424f05824c4b81fa
cjlovering <cjlovering@130.215.14.76>
2017-01-14 23:06:33 -0500

    story

--- story.py
+++ story.py
@@ -6,6 +6,8 @@ def handle_story(state, sender_id, message_in):
     # build story
     potential = ["Let me think...", "I've got a good one...", "I made this one up myself...", "This happened a long time ago..."]
     send_message(sender_id, potential[random.randint(0,3)])
+
+    # 
     story = story_gen(message_in)
     # send story
     # format story
