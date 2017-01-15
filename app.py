4b5ec583493def9892e5cf233b9239a1ab85fdbc
saahilclaypool <saahil@claypools.org>
2017-01-14 23:17:49 -0500

    remove double import

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from flask import Flask, request
 
 from weather import handle_weather
 from story import handle_story
-from story_gen import *
 from image_search import *
 from messages import *
 from constants import *
