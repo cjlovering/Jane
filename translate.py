b4a7e093aa2a5264ffbcfcf671275f9ea1c69f71
1nkling <dlsong@wpi.edu>
2017-01-15 00:38:45 -0500

    transl

--- translate.py
+++ translate.py
@@ -1,6 +1,6 @@
 from yandex_translate import YandexTranslate
 
-dict = {'english': 'en' 'spanish': 'es', 'italian': 'it' 'greek': 'el' 'russian': 'ru' 'swedish':'sv' 'dutch':'nl'}
+dict = {'english': 'en', 'spanish': 'es', 'italian': 'it', 'greek': 'el', 'russian': 'ru', 'swedish':'sv', 'dutch':'nl'}
 
 def handle_transl(message_in, target):
     #r = requests.get(https://translate.yandex.net/api/v1.5/tr.json/translate?key=<trnsl.1.1.20170115TO20150Z.5a7ee6b315a11aa3.083f6b10747c2f39183936fc5ec47ebccff59370
