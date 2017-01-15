1374877aabb097409033b07b5302cd91a2ab5614
saahilclaypool <saahil@claypools.org>
2017-01-14 23:48:30 -0500

    memmory mngt

--- story.py
+++ story.py
@@ -2,10 +2,12 @@ from messages import *
 import random
 from story_gen import story_gen, get_sequence
 
-pattern, model, int_to_char, n_vocab, n_chars = story_gen()
 
 
 def handle_story(state, sender_id, message_in):
+    send_message(sender_id, "One second, looking through the libraries...")
+    pattern, model, int_to_char, n_vocab, n_chars = story_gen()
+    log("Finished building model...")
     # build story
     potential = ["Let me think...", "I've got a good one...", "I made this one up myself...", "This happened a long time ago..."]
     send_message(sender_id, potential[random.randint(0,3)])
